b832c660284f30f410c71c1c61a2afb94a47a20a
BOYA SAITEJA <boyasaiteja007@gmail.com>
2021-08-18 15:11:50 +0530

    done

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,17 @@
 // import using require
-
+var Shape = require("./shape");
 // declare class
+
+class Circle extends Shape {
+    constructor(props) {
+        super(props);
+    }
+
+    calculateArea() {
+        
+    }
+}
 
 
 // export class using module.exports
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
+import React from "react";
 // declare class
+class Shape extends React.Component {
+    constructor(props) {
+        super(props);
+        this.color = "red";
+    }
+    drawShape() {
 
+    }
+    calculateArea(){
+
+    }
+}
 // export class using module.exports
+module.exports = Shape;
